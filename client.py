540861bc8173d2af92ee43d4d7a35e1fd09a4eea
sushi <64286083+sushiroles@users.noreply.github.com>
2025-01-06 19:38:47 +0100

    de-hardcoded values and clean-up

--- client.py
+++ client.py
@@ -5,10 +5,11 @@ from discord.ext import tasks
 from random import randint
 from asyncio import *
 from Random.time import *
+import configparser
 
 from APIs import *
 
-import configparser
+
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -18,6 +19,8 @@ tokens.read('tokens.ini')
 
 vars = configparser.ConfigParser()
 vars.read('vars.ini')
+
+
 
 class Client(discord.Client):
 	def __init__(self, *args, **kwargs):
@@ -41,25 +44,27 @@ async def on_ready():
 		client.synced = True
 	if not check_for_stream.is_running():
 		check_for_stream.start()
-	genchat = await client.fetch_channel(1287081301542830081)
+	announcements_channel = await client.fetch_channel(int(config['channels']['stream_announcements']))
+	await announcements_channel.send('-# (test :3)')
 
 
 
 @tasks.loop(seconds = 5)
 async def check_for_stream():
 	stream_data = await twitch.is_streaming()
-	genchat = await client.fetch_channel(1287081301542830081)
+	vix_data = await twitch.get_profile()
+	announcements_channel = await client.fetch_channel(int(config['channels']['stream_announcements']))
 	if stream_data['is_live'] == True and vars['stream_notifs']['is_live'] == 'False':
 		embed = discord.Embed(
 			title = f'{stream_data['title']}',
-            url = "https://twitch.tv/vixrtv",
-            colour = 0x6441a5
+			url = "https://twitch.tv/vixrtv",
+			colour = 0x6441a5
 		)
-		embed.set_author(name = f"Vix is live and streaming {stream_data['activity']}!")
-		embed.set_image(url = stream_data['thumbnail_url'].replace('{width}','1280').replace('{height}','720'))
+		embed.set_author(name = f"Vix is live and streaming {stream_data['activity']}!", icon_url = vix_data['profile_image_url'],)
+		embed.set_image(url = stream_data['thumbnail_url'].replace('{width}', config['stream_announcements']['tres_w']).replace('{height}', config['stream_announcements']['tres_h']))
 		embed.set_footer(text = "~VixrBot :3")
 
-		await genchat.send('<@&1325615374913376286> >>> https://twitch.tv/vixrtv <<<', embed = embed)
+		await announcements_channel.send(f'<@&{config['roles']['stream_notifs_role_id']}>   >>> https://twitch.tv/vixrtv <<<', embed = embed)
 
 		vars.set('stream_notifs', 'is_live', 'True')
 		with open('vars.ini', 'w') as vars_file:
